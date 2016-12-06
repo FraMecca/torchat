ef6140ae065cefc21e3d524961d61265b12d9b7a
PesceWanda <me@francescomecca.eu>
2016-12-06 22:26:28 +0100

    client ancora

--- src/client.py
+++ src/client.py
@@ -33,5 +33,17 @@ def main (portno):
 
 if __name__ == '__main__':
     from sys import argv
-    while (1): 
-        main (argv[1])
+    import time
+    portno = 8000
+    # while (1): 
+        # main (argv[1])
+    s = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+    s.connect (("localhost", int (8000)))
+    cmd = "SEND"
+    ip = "hisqz2dygtajbnf7.onion"
+    msg = str (time.time ())
+    jsonStr = ''.join (['{"cmd": "', cmd, '","id":"', ip, '", "portno": ', str(portno), ',"msg": "', msg, '"}'])
+    print ("Sending encoded json: ", jsonStr)
+
+# jsonStr = b'{"cmd": ' + cmd + b',"id":"Clientpy", "porto": ' + bytes(portno) + b',"msg": ' + msg + b'}'
+    print (s.send (bytes (jsonStr, 'utf-8')))
