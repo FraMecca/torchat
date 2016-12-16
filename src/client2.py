8ee50a64b39e1338dca1ca62b7fc614bf81ce8d6
fragsornotfrags <france.galla@live.com>
2016-12-16 12:51:01 +0100

    multithread

--- src/client2.py
+++ src/client2.py
@@ -1,7 +1,26 @@
 import json
 import socket
 import readline
-# import thread
+from time import sleep
+from threading import Thread 
+
+def update_routine(peerList, i, portno):
+    # this function queries the server for unread messages
+    # it runs until no messages from the given peer are left
+    # then waits half a second and queries again
+    while True:
+        j = create_json (cmd='UPDATE', msg=peerList[int (i)])
+        resp = send_to_mongoose (j, portno)
+        # the json is not printed if no messages are received
+        if resp['cmd'] == 'END':
+            sleep(0.5)
+        else:
+            print (resp) # we NEED a function that prints the json in a nicer way
+
+def input_routine(): # tutta tua mecca
+    while True:
+        print ("mecca ghei")
+        sleep(2)
 
 def create_json (cmd='', msg=''):
     if cmd == '':
@@ -24,34 +43,28 @@ def send_to_mongoose (j, portno):
     return resp
 
 def main (portno):
+
+    # first of all, ask for a list of peers with pending messages
     j = create_json (cmd='GET_PEERS')
     resp = send_to_mongoose (j, portno) 
     peerList = resp['msg'].split (',')
-    if peerList[0] == '':
+
+    if peerList[0] == '': # no peers have written you! 
         print ("No peers")
         exit (0)
     else:
         i = 0
-        for id in peerList:
+        for id in peerList: # print them all with an integer id associated
             print (str (i) + '.', peerList[i])
             i+=1
-        print ("Choose one id: ", end = '')
-        i = input ()
-        while True:
-            j = create_json (cmd='UPDATE', msg=peerList[int (i)])
-            resp = send_to_mongoose (j, portno)
-            print (resp) 
-            if resp['cmd'] == 'END':
-                break
 
-
-
-
-
-
-
-
-
+    # here we use one thread to update unread messages, another that sends
+    print ("Choose one id: ", end = '')
+    i = input ()
+    t1 = Thread(target=update_routine, args=(peerList, i, portno))
+    t2 = Thread(target=input_routine, args=()) #mecca metti qui tutti gli args che ti servono in input_routine separati da vigola
+    t1.start()
+    t2.start()
 
 if __name__ == '__main__':
     from sys import argv
