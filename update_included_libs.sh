3f9e1d3133ce163698a8d8b5c31a9f004a53613d
PesceWanda <me@francescomecca.eu>
2016-12-20 19:39:08 +0100

    update included libs

---
+++ update_included_libs.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/emilk/loguru/master/loguru.hpp -O include/loguru.hpp
+wget https://raw.githubusercontent.com/nlohmann/json/develop/src/json.hpp -O include/json.hpp
+wget https://raw.githubusercontent.com/cesanta/mongoose/master/mongoose.h -O include/mongoose.h
+wget https://raw.githubusercontent.com/cesanta/mongoose/master/mongoose.c -O include/moongose.c
