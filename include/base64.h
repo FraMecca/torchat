67d5a8c4dc2f2677577a6893913fdc9fe40d19b6
PesceWanda <me@francescomecca.eu>
2017-03-20 10:53:06 +0100

    base64

---
+++ include/base64.h
@@ -0,0 +1,7 @@
+#include <string.h> // memset
+#include "mem.h" // MALLOC
+unsigned char * base64_decode(const unsigned char *src, size_t len,
+			      size_t *out_len);
+
+unsigned char * base64_encode(const unsigned char *src, size_t len,
+			      size_t *out_len);
